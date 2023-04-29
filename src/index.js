c69c8b219e88ef7ed7036bf8cfe157b38a97407a
iamstillcath <catherine2lucia@gmail.com>
2023-04-30 00:03:31 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const express = require('express');
+const mongoose= require("mongoose")
 const app =express();
-
+const dotenv= require('dotenv')
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get('/',function(req,res){
     res.send('this is babck');
