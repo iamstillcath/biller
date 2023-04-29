8755eb679f5bf117ace7dfa2fbb51db7dd4d2df4
iamstillcath <catherine2lucia@gmail.com>
2023-04-29 18:21:41 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -1,16 +1,12 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const express = require('express')
+const app =express();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const PORT = process.env.PORT || 5000;
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+app.get('/',function(req,res){
+    res.send('this is babck');
+  });
+
+app.listen(PORT, () => console.log(`app is running on port ${PORT}`))
 
 module.exports = app;
